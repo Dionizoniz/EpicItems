a28a748035bf401c0535612e9c782929f18340a0
Dionizoniz <dionizoniz@gmail.com>
2024-11-20 20:33:19 +0100

    Add graphic setup for Item categories.

---
+++ Assets/Scripts/UI/Setup/IItemSetup.cs
@@ -0,0 +1,10 @@
+﻿using EpicItems.Core.DataServer;
+using UnityEngine;
+
+namespace EpicItems.UI.Setup
+{
+    public interface IItemSetup
+    {
+        Sprite TryFindSpriteBy(DataItem.CategoryType categoryType);
+    }
+}
---
+++ Assets/Scripts/UI/Setup/ItemSetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using EpicItems.Core.DataServer;
+using EpicItems.Core.Engine;
+using EpicItems.UI.ItemPanel;
+using UnityEngine;
+
+namespace EpicItems.UI.Setup
+{
+    [CreateAssetMenu(menuName = nameof(ItemSetup), fileName = nameof(ItemSetup), order = 0)]
+    public class ItemSetup : ExtendedScriptableObject, IItemSetup
+    {
+        [SerializeField]
+        private List<CategoryData> _categories = new();
+
+        public Sprite TryFindSpriteBy(DataItem.CategoryType categoryType)
+        {
+            Sprite result = null;
+
+            foreach (var category in _categories)
+            {
+                if (category.CategoryType == categoryType)
+                {
+                    result = category.CategorySprite;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
