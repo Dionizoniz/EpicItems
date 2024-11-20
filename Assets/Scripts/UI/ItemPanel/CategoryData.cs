5e017baa0a2451bb87dc4f854716eb63ce89bd82
Dionizoniz <dionizoniz@gmail.com>
2024-11-20 20:44:34 +0100

    Add logic and setup for ItemController and prefab.

---
+++ Assets/Scripts/UI/ItemPanel/CategoryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EpicItems.Core.DataServer;
+using UnityEngine;
+
+namespace EpicItems.UI.ItemPanel
+{
+    [Serializable]
+    public class CategoryData
+    {
+        [SerializeField]
+        private DataItem.CategoryType _categoryType;
+        [SerializeField]
+        private Sprite _categorySprite;
+
+        public DataItem.CategoryType CategoryType => _categoryType;
+        public Sprite CategorySprite => _categorySprite;
+    }
+}
