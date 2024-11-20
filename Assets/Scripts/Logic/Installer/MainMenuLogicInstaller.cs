752c099ffd0b25d6737516e678be99fe72404334
Dionizoniz <dbijas@icecodegames.com>
2024-11-20 14:56:01 +0100

    Add the MainMenuLogic installer to the scene and install base prefabs and itemsProvider

--- Assets/Scripts/Logic/Installer/MainMenuLogicInstaller.cs
+++ Assets/Scripts/Logic/Installer/MainMenuLogicInstaller.cs
@@ -1,9 +1,19 @@
 ﻿using EpicItems.Core.Entities.Installers;
+using EpicItems.Logic.Items;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace EpicItems.Logic.Installer
 {
     public class MainMenuLogicInstaller : BaseInstaller, IMainMenuLogicInstaller
     {
+        [SerializeField]
+        private EventSystem _eventSystem;
+        [SerializeField]
+        private Camera _camera;
+
+        private IItemsProvider _itemsProvider;
+
         private void Awake()
         {
             SpawnSystems();
@@ -12,10 +22,13 @@ namespace EpicItems.Logic.Installer
 
         private void SpawnSystems()
         {
+            Instantiate(_eventSystem);
+            Instantiate(_camera);
         }
 
         private void CreateInstances()
         {
+            _itemsProvider = new ItemsProvider();
         }
 
         protected override void Start()
