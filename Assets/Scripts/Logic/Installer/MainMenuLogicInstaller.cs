0e1376a26ef64276261f209d36addfa79bb162d5
Dionizoniz <dbijas@icecodegames.com>
2024-11-20 14:51:16 +0100

    Create logic installer

---
+++ Assets/Scripts/Logic/Installer/MainMenuLogicInstaller.cs
@@ -0,0 +1,31 @@
+﻿using EpicItems.Core.Entities.Installers;
+
+namespace EpicItems.Logic.Installer
+{
+    public class MainMenuLogicInstaller : BaseInstaller, IMainMenuLogicInstaller
+    {
+        private void Awake()
+        {
+            SpawnSystems();
+            CreateInstances();
+        }
+
+        private void SpawnSystems()
+        {
+        }
+
+        private void CreateInstances()
+        {
+        }
+
+        protected override void Start()
+        {
+            InitializeSystems();
+            base.Start();
+        }
+
+        private void InitializeSystems()
+        {
+        }
+    }
+}
