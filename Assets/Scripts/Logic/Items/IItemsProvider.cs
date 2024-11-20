dca42d4851d469ec4486778cdab5fe8a6e3c2819
Dionizoniz <dbijas@icecodegames.com>
2024-11-20 14:38:30 +0100

    Add ItemsProvider API

--- Assets/Scripts/Logic/Items/IItemsProvider.cs
+++ Assets/Scripts/Logic/Items/IItemsProvider.cs
@@ -1,6 +1,14 @@
-﻿namespace EpicItems.Logic.Items
+﻿using System;
+using System.Collections.Generic;
+using EpicItems.Core.DataServer;
+
+namespace EpicItems.Logic.Items
 {
     public interface IItemsProvider
     {
+        event Action OnDataLoaded;
+
+        IList<DataItem> Items { get; }
+        bool IsDataAvailable { get; }
     }
 }
