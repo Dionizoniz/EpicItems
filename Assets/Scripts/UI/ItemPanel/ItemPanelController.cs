90ac540968f00f19abcbfb8c6297777baf9cec05
Dionizoniz <dionizoniz@gmail.com>
2024-11-20 20:44:59 +0100

    Add initial draft logic for items spawning.

--- Assets/Scripts/UI/ItemPanel/ItemPanelController.cs
+++ Assets/Scripts/UI/ItemPanel/ItemPanelController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using EpicItems.Core.Entities.MVC;
 using EpicItems.Logic.Items;
+using UnityEngine;
 
 namespace EpicItems.UI.ItemPanel
 {
@@ -11,9 +12,28 @@ namespace EpicItems.UI.ItemPanel
 
         private IItemsProvider _itemsProvider;
 
+        // TEST CODE
+        [SerializeField]
+        private ItemController _itemToSpawn;
+        [SerializeField]
+        private Transform _root;
+
         public void InjectData(IItemsProvider itemsProvider)
         {
             _itemsProvider = itemsProvider;
+
+            // TEST CODE
+            _itemsProvider.OnDataLoaded += SpawnItems;
+        }
+
+        // TEST CODE
+        private void SpawnItems()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var item = Instantiate(_itemToSpawn, _root);
+                item.Initialize(i + 1, _itemsProvider.Items[i]);
+            }
         }
 
         public void ShowPanel()
