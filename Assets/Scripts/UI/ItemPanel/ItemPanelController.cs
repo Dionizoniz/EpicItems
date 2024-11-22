792db9aeeb472ee8e1df9c2ce96f4698c8ea02b8
Dionizoniz <dbijas@icecodegames.com>
2024-11-22 14:30:30 +0100

    Move item spawn code to view.

--- Assets/Scripts/UI/ItemPanel/ItemPanelController.cs
+++ Assets/Scripts/UI/ItemPanel/ItemPanelController.cs
@@ -7,33 +7,18 @@ namespace EpicItems.UI.ItemPanel
 {
     public class ItemPanelController : Controller<ItemPanelModel, ItemPanelView>, IItemPanelController
     {
-        public event Action OnClose = delegate
-                                      { };
+        public event Action OnClose = delegate { };
 
         private IItemsProvider _itemsProvider;
 
         // TEST CODE
-        [SerializeField]
-        private ItemController _itemToSpawn;
-        [SerializeField]
-        private Transform _root;
+        
 
         public void InjectData(IItemsProvider itemsProvider)
         {
             _itemsProvider = itemsProvider;
 
-            // TEST CODE
-            _itemsProvider.OnDataLoaded += SpawnItems;
-        }
-
-        // TEST CODE
-        private void SpawnItems()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                var item = Instantiate(_itemToSpawn, _root);
-                item.Initialize(i + 1, _itemsProvider.Items[i]);
-            }
+            _view.InjectData(_itemsProvider);
         }
 
         public void ShowPanel()
--- Assets/Scripts/UI/ItemPanel/ItemPanelView.cs
+++ Assets/Scripts/UI/ItemPanel/ItemPanelView.cs
@@ -1,17 +1,57 @@
-﻿using EpicItems.Core.Entities.MVC;
+﻿using System;
+using EpicItems.Core.Entities.MVC;
+using EpicItems.Logic.Items;
+using UnityEngine;
 
 namespace EpicItems.UI.ItemPanel
 {
     public class ItemPanelView : View
     {
+        [SerializeField]
+        private ItemController _itemToSpawn;
+        [SerializeField]
+        private Transform _root;
+
+        private IItemsProvider _itemsProvider;
+
+        public void InjectData(IItemsProvider itemsProvider)
+        {
+            _itemsProvider = itemsProvider;
+        }
+
         public void ShowPanel()
         {
+            if (_itemsProvider.IsDataAvailable)
+            {
+                SpawnItems();
+            }
+            else
+            {
+                _itemsProvider.OnDataLoaded += SpawnItems;
+            }
+
             _gameObject.SetActive(true);
+        }
+
+        private void SpawnItems()
+        {
+            _itemsProvider.OnDataLoaded -= SpawnItems;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var item = Instantiate(_itemToSpawn, _root);
+                item.Initialize(i + 1, _itemsProvider.Items[i]);
+            }
         }
 
         public void ClosePanel()
         {
             _gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            _itemsProvider.OnDataLoaded -= SpawnItems;
         }
     }
 }
