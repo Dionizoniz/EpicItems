e49719b756e1a626244dee26fbd3339eb8413980
Dionizoniz <dionizoniz@gmail.com>
2024-11-23 07:20:44 +0100

    Add a component to rotate the element.

---
+++ Assets/Scripts/Core/Utilities/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using EpicItems.Core.Engine;
+using UnityEngine;
+
+namespace EpicItems.Core.Utilities
+{
+    public class Rotate : ExtendedMonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _axis = Vector3.forward;
+        [SerializeField]
+        private float _speed = 100f;
+
+        private void Update()
+        {
+            float offset = _speed * Time.deltaTime;
+            _transform.Rotate(_axis, offset);
+        }
+    }
+}
