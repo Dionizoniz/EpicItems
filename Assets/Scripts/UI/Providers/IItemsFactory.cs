f136dc788937eb5226176a2d66e66e340d765b4e
Dionizoniz <dionizoniz@gmail.com>
2024-11-23 07:47:11 +0100

    Add items factory.

---
+++ Assets/Scripts/UI/Providers/IItemsFactory.cs
@@ -0,0 +1,11 @@
+﻿using EpicItems.UI.ItemPanel;
+using UnityEngine;
+
+namespace EpicItems.UI.Providers
+{
+    public interface IItemsFactory
+    {
+        ItemController CreateArrowInstance(ItemController itemPrefab, Transform itemsRoot);
+        void ReturnArrowInstance(ItemController item);
+    }
+}
---
+++ Assets/Scripts/UI/Providers/ItemsFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using EpicItems.UI.ItemPanel;
+using UnityEngine;
+
+namespace EpicItems.UI.Providers
+{
+    public class ItemsFactory : IItemsFactory
+    {
+        private readonly Stack<ItemController> _itemsPool = new();
+
+        public ItemController CreateArrowInstance(ItemController itemPrefab, Transform itemsRoot)
+        {
+            ItemController item = _itemsPool.Count > 0 ? _itemsPool.Pop() : Object.Instantiate(itemPrefab, itemsRoot);
+            item._gameObject.SetActive(true);
+
+            return item;
+        }
+
+        public void ReturnArrowInstance(ItemController item)
+        {
+            item._gameObject.SetActive(false);
+            _itemsPool.Push(item);
+        }
+    }
+}
